7d2e5bb9ca4fbbc97cf5218c5188a6bd28b89a06
IoSonoN <crudocommunityofficial@gmail.com>
2023-11-21 19:43:07 +0100

    cambio

--- src/App.js
+++ src/App.js
@@ -1,83 +1,57 @@
 import { useRef } from "react";
+import Cookies from 'universal-cookie';
+//import { Jwt } from "jsonwebtoken";
 
 function App() {
   const inputRef = useRef(null)
   const resultRef = useRef(null)
 
   const executeLlm = async () => {
-    resultRef.current.innerHTML = "Working on it..."
-    const question = inputRef.current.value
-    const question_template = `Question: ${question}. Is this a question regarding legal matter or legal advices or informations about the law? Answer specificaly with "Yes" or "No" only`
+    const cookies = new Cookies();
+    let messages = cookies.get("history")
 
-    /*const req = await fetch(
-      "https://api.openai.com/v1/chat/completions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer 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`
-        },
-        body: JSON.stringify({
-          "model": "gpt-4",
-          "messages": [
-            { "role": "system", "content": "" },
-            { "role": "assistant", "content": "" },
-            { "role": "user", "content": question_template }
-          ],
-          "temperature": 0.05,
-          "max_tokens": 256,
-          "top_p": 1,
-          "frequency_penalty": 0,
-          "presence_penalty": 0
-        })
+    let history = ""
+
+    if (messages) {
+      history = "Tieni a mente questa conversazione prima di rispondere, considerala come un'interazione tra te e l'utente a cui stai rispondento. \n"
+      let obj = messages
+
+      let array = obj.messages
+
+      for (let i = 0; i < array.length; i++) {
+        let prefix = array[i].isAi ? "Tu: " : "Utente: "
+        history += prefix + array[i].content + "\n"
       }
-    )
+    }
 
-    const res = await req.json()
-    let result = res.choices[0].message.content
-
-    console.log("First result: ", result)*/
-
+    resultRef.current.innerHTML = "Ci sto lavorando..."
+    const question = inputRef.current.value
     const review_question = `
-      LegalOne is not a team of experts but an app that puts people in contact with lawyers
+      ${history}
 
-      ${question}
+      Ecco a te la nuova domanda dell'utente, Riassuntizza i concetti chiesti senza trascurare però i dettagli più importanti, esegui i collegamenti, se richiesti, contestualizzandoli ad eventi storici attuali e passati.
 
-      point out nicely to consult a lawyer from LegalOne app to answer the question more precisely. 
+      Domanda: ${question}
     `
-  
+
     const req2 = await fetch(
       "https://api.openai.com/v1/chat/completions",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer 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`
+          "Authorization": `Bearer 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`
         },
         body: JSON.stringify(
-          /*{
-            "model": "gpt-4",
-            "messages": [
-              { "role": "system", "content": `Don't say anything about yourself like "I'm an AI developed by OpenAI and I don't have a gender", just answer like specified below. Don't answer to anything that is not a legal matter or legal advice or informations about the law. If the question above is not a legal matter or legal advice or informations about the law then precisely type down "I cannot answer this question" and point out nicely that you can answer only legal matters without specifying that the question is not a legal matter or legal advice or informations about the law
-              Also invite the user to ask legal matters questions only` },
-              { "role": "assistant", "content": "" },
-              { "role": "user", "content": review_question }
-            ],
-            "temperature": 0.05,
-            "max_tokens": 256,
-            "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0
-          }*/
           {
             "model": "gpt-4",
             "messages": [
               { "role": "system", "content": `Comportati come Jarvis, l'intelligenza artificiale di Tony Stark` },
               { "role": "assistant", "content": "" },
-              { "role": "user", "content": question }
+              { "role": "user", "content": review_question }
             ],
             "temperature": 0.05,
-            "max_tokens": 256,
+            "max_tokens": 5000,
             "top_p": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0
@@ -89,6 +63,37 @@ function App() {
     const res2 = await req2.json()
     console.log("Final result: ", res2.choices[0].message.content)
     resultRef.current.innerHTML = res2.choices[0].message.content
+
+    if (messages) {
+      let obj = messages
+
+      obj.messages.push([
+        {
+          isAi: false,
+          content: question
+        },
+        {
+          isAi: true,
+          content: res2.choices[0].message.content
+        }
+      ])
+
+      cookies.set("history", JSON.stringify(obj))
+    }
+    else {
+      cookies.set("history", JSON.stringify({
+        messages: [
+          {
+            isAi: false,
+            content: question
+          },
+          {
+            isAi: true,
+            content: res2.choices[0].message.content
+          }
+        ]
+      }))
+    }
   }
 
   return (
@@ -100,11 +105,17 @@ function App() {
         className="input_text"
       />
 
+      <p>
+        Risposta
+      </p>
+
       <p
         ref={resultRef}
         className="result"
+        style={{
+          marginTop: 20
+        }}
       >
-
       </p>
 
       <button
@@ -112,7 +123,20 @@ function App() {
         onClick={executeLlm}
         className="button_prompt"
       >
-        Prompta
+        Chiedi
+      </button>
+
+      <button
+        type="button"
+        onClick={() => {
+          resultRef.current.innerHTML = ""
+        }}
+        className="button_prompt"
+        style={{
+          marginBottom: 20
+        }}
+      >
+        Pulisci
       </button>
     </div>
   );
